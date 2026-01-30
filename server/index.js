2a8b7202ef7bf88ddf75f21a32415779b4d77b03
nickkni <nickkni@tencent.com>
2026-01-30 15:02:59 +0800

    Update server default port to 3005

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
 // Socket.io
 socketHandler(io, supabase);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
